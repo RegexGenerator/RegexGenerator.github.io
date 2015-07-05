e0e67a5e6e9793c6b5b09858cfe16c5c56b0f844
ReinRaus <reinraus.nax@gmail.com>
2015-07-05 19:26:58 +0400

    Добавлен анализатор формул. Временно.

--- files/main.js
+++ files/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	prepareAll();
 	prepareTab1();
+	prepareTab2();
 });
 
 function prepareAll() {
@@ -41,3 +42,32 @@ function prepareTab1() {
 		console.log( re );
 	} ); 
 };
+
+function prepareTab2() {
+	function defined() {
+		for ( var i=0; i < arguments.length; i++ )
+			if ( typeof( arguments[i] ) !== "undefined" ) return arguments[i];
+		return arguments[0];
+	};
+	
+	$( "#tabs-2 #run" ).click( function() {
+		var text = $( "#tabs-2 #text" ).val();
+		var re = /^(?:(-?\d+)([*\/+-])(-?\d+)$|(.*?)(\d+)([*\/])(-?\d+)|(.*?)(\d+)([+-])(-?\d+))/;
+		var oldLength = -1;
+		while ( !/^-?\d+$/.test( text ) && oldLength != text.length ) {
+			oldLength = text.length;
+			text = text.replace( /[+-]{2,}/g, function( val ) {
+				return ( val.replace( /\+/g, "").length % 2 == 0 ) ? "+" : "-";
+			} );
+			if ( text.length != oldLength ) console.log( text );
+			text = text.replace( re, function() {
+				var g1 = defined( arguments[1], arguments[5], arguments[9] );
+				var g2 = defined( arguments[2], arguments[6], arguments[10] );
+				var g3 = defined( arguments[3], arguments[7], arguments[11] );
+				var before = defined( arguments[4], arguments[8], "" );
+				return before + eval( g1 + g2 + g3 );
+			} );
+			console.log( text );
+		}
+	} );
+}
